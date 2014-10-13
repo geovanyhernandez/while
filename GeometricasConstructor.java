60871bdd743943df8b892721f9cfb6117748ea6d
giovanyhernandez <ghernandezes14dw@ikzubirimanteo.com>
2014-10-13 18:41:45 +0200

    Update GeometricasConstructor.java

--- GeometricasConstructor.java
+++ GeometricasConstructor.java
@@ -39,23 +39,15 @@ else
 
 double altura = 0;
 double base = 0;
-double superficie;
 
-	Rectangulo operaciones = new Rectangulo(altura, base);
- 
-	superficie = operaciones.Superficie();
-
-
-//	Scanner sc = new Scanner(System.in);
 	System.out.print("\ningresa la altura del rectangulo : ");
 	altura = sc.nextDouble();
 
 	System.out.print("\ningresa la base del rectangulo : ");
         base = sc.nextDouble();
-
-	operaciones.setBase(base);
-	operaciones.setAltura(altura);
-
+        
+	Rectangulo operaciones = new Rectangulo(altura, base);
+	
 	System.out.println("\nla base es : " + operaciones.Superficie());
 	System.out.println("\nel perimetro es : " + operaciones.Perimetro());
 	
